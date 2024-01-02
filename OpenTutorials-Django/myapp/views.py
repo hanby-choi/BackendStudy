3434588244c9e9c74b0def354040f747b67cab27
Hanby Choi <hanby.choi@ewhain.net>
2024-01-02 19:39:02 +0900

    [생활코딩] Python Django Web Framework 1-5

---
+++ OpenTutorials-Django/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    return HttpResponse('Welcome!')
+
+def create(request):
+    return HttpResponse('Create!')
+
+def read(request, id):
+    return HttpResponse('Read!' + id)
