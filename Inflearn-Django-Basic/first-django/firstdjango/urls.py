308ad2d4c40105579b6f77b6a77852bd382a9fde
Hanby Choi <hanby.choi@ewhain.net>
2024-01-18 01:05:56 +0900

    [Inflearn] 파이썬으로 장고 공략하기: 입문 5. ORM

--- Inflearn-Django-Basic/first-django/firstdjango/urls.py
+++ Inflearn-Django-Basic/first-django/firstdjango/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from first import views
 
 urlpatterns = [
     path('first/', include('first.urls')),
     path('second/', include('second.urls')),
+    path('third/', include('third.urls')),
     path("admin/", admin.site.urls),
 ]
