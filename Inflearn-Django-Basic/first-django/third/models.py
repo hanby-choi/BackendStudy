6cabf7aa30f6e30fc862e304abdcbb23abe988cb
Hanby Choi <hanby.choi@ewhain.net>
2024-01-25 12:32:41 +0900

    [Inflearn] 파이썬으로 장고 공략하기: 입문 7. Relation

--- Inflearn-Django-Basic/first-django/third/models.py
+++ Inflearn-Django-Basic/first-django/third/models.py
@@ -6,3 +6,11 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    point = models.IntegerField()
+    comment = models.CharField(max_length=500)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
